a7287fde549fa1b017a46a62e4b78a0efec978fe
Jelte Dirks <j.dirks@outlook.com>
2019-04-23 18:15:05 +0200

    short demonstration of 'schade melden' page

--- src/app/pages/schade-melden/schade-melden.page.ts
+++ src/app/pages/schade-melden/schade-melden.page.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {GeolocationPlugin, GeolocationPosition, Plugins, CameraResultType, CameraPlugin, CameraPhoto} from '@capacitor/core';
 
 @Component({
-  selector: 'app-schade-melden',
-  templateUrl: './schade-melden.page.html',
-  styleUrls: ['./schade-melden.page.scss'],
+    selector: 'app-schade-melden',
+    templateUrl: './schade-melden.page.html',
+    styleUrls: ['./schade-melden.page.scss'],
 })
 export class SchadeMeldenPage implements OnInit {
 
-  constructor() { }
+    private geo: GeolocationPlugin;
+    private cam: CameraPlugin;
+    public location;
+    public imageURIS: string[];
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
+
+    ngOnInit() {
+        this.geo = Plugins.Geolocation;
+        this.cam = Plugins.Camera;
+    }
+
+    async onGeoRequest() {
+        await this.geo.getCurrentPosition({enableHighAccuracy: true}).then((pos: GeolocationPosition) => {
+            this.location = pos.coords;
+            console.log('location set', this.location);
+        });
+    }
+
+    async onCameraRequest() {
+        await this.cam.getPhoto({
+            quality: 90,
+            allowEditing: false,
+            resultType: CameraResultType.Uri
+        }).then((photo: CameraPhoto) => {
+            this.imageURIS.push(photo.webPath);
+            document.getElementById('demo-image').setAttribute('src', photo.webPath);
+        });
+    }
 
 }
