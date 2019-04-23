e01fed6f380fca02898fea9158eaae48ccf78f19
Jelte Dirks <j.dirks@outlook.com>
2019-04-23 20:32:26 +0200

    add hulp nodig page

---
+++ src/app/pages/hulp-nodig/hulp-nodig.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hulp-nodig',
+  templateUrl: './hulp-nodig.page.html',
+  styleUrls: ['./hulp-nodig.page.scss'],
+})
+export class HulpNodigPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
