9b179f2b08fa3aee7e182be109eb58d48c6eff08
Jelte Dirks <j.dirks@outlook.com>
2019-04-23 23:36:10 +0200

    log user changes

--- src/app/firebase.service.ts
+++ src/app/firebase.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@angular/core';
+import {Injectable, OnInit} from '@angular/core';
+import {AngularFirestore} from '@angular/fire/firestore';
+import {AngularFireAuth} from '@angular/fire/auth';
+import {User} from 'firebase';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
-export class FirebaseService {
+export class FirebaseService implements OnInit {
 
-  constructor() { }
+    constructor(private db: AngularFirestore,
+                private auth: AngularFireAuth) {
+    }
+
+    ngOnInit(): void {
+        this.auth.user.subscribe((u: User) => {
+            console.log(u);
+        });
+    }
 }
